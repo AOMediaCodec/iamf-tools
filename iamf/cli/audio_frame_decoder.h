94add2657d412a7b9d1bff47a4c2fc8eec49403b
Googler <copybara-servicebot@google.com>
2024-08-06 13:29:23 -0400

    Add ReconGainInfoParameterData to DecodedAudioFrame.
    This will be used to pass through recon gain parameters that will be used post decoding (and demixing).
    
    PiperOrigin-RevId: 660008365

--- iamf/cli/audio_frame_decoder.h
+++ iamf/cli/audio_frame_decoder.h
@@ -24,6 +24,7 @@
 #include "iamf/cli/codec/decoder_base.h"
 #include "iamf/obu/codec_config.h"
 #include "iamf/obu/demixing_info_param_data.h"
+#include "iamf/obu/parameter_block.h"
 
 namespace iamf_tools {
 
@@ -41,6 +42,9 @@ struct DecodedAudioFrame {
 
   // Down-mixing parameters used to create this audio frame.
   DownMixingParams down_mixing_params;
+
+  // Recon gain parameters used to adjust the gain of this audio frame.
+  ReconGainInfoParameterData recon_gain_info_param_data;
 
   // The audio element with data associated with this frame.
   const AudioElementWithData* audio_element_with_data;
