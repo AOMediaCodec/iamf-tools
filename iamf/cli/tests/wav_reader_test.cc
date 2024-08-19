0cb2194c6217375b9110f6c9a8ef8a226bbef4b1
jwcullen <jwcullen@google.com>
2024-08-19 18:58:33 -0400

    `WavReader`: Test that it works fine directly on ADM files.
    PiperOrigin-RevId: 665021990

--- iamf/cli/tests/wav_reader_test.cc
+++ iamf/cli/tests/wav_reader_test.cc
@@ -22,6 +22,7 @@
 
 // Placeholder for get runfiles header.
 #include "absl/status/status_matchers.h"
+#include "absl/strings/string_view.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "iamf/cli/tests/cli_test_utils.h"
@@ -32,6 +33,59 @@ namespace {
 using ::absl_testing::IsOk;
 
 constexpr size_t kArbitraryNumSamplesPerFrame = 1;
+constexpr absl::string_view kAdmBwfWithOneStereoAndOneMonoObject(
+    "RIFF"
+    "\xf5\x00\x00\x00"  // Size of `RIFF` chunk (the whole file).
+    "WAVE"
+    "fmt "
+    "\x10\x00\x00\x00"  // Size of the `fmt ` chunk.
+    "\x01\x00"          // Format tag.
+    "\x03\x00"          // Number of channels.
+    "\x80\xbb\x00\x00"  // Sample per second
+    "\x00\x65\x04\x00"  // Bytes per second = [number_of_channels *
+                        // ceil(bits_per_sample / 8) * sample_per_second].
+    "\x06\x00"          // Block align = [number of channels * bits per sample]
+    "\x10\x00"          // Bits per sample.
+    "data"
+    "\x0c\x00\x00\x00"  // Size of `data` chunk.
+    "\x01\x23"          // Sample[0] for object[0] L.
+    "\x45\x67"          // Sample[0] for object[0] R.
+    "\xaa\xbb"          // Sample[0] for object[1] M.
+    "\x89\xab"          // Sample[1] for object[0] L.
+    "\xcd\xef"          // Sample[1] for object[0] R.
+    "\xcc\xdd"          // Sample[1] for object[1] M.
+    "axml"
+    "\xbd\x00\x00\x00"  // Size of `axml` chunk.
+    "<topLevel>"
+    "<audioObject>"
+    "<audioTrackUIDRef>L</audioTrackUIDRef>"
+    "<audioTrackUIDRef>R</audioTrackUIDRef>"
+    "</audioObject>"
+    "<audioObject>"
+    "<audioTrackUIDRef>M</audioTrackUIDRef>"
+    "</audioObject>"
+    "</topLevel>",
+    253);
+
+void CreateFileFromStringView(absl::string_view file_contents,
+                              absl::string_view file_path_suffix,
+                              std::string& file_name) {
+  file_name = GetAndCleanupOutputFileName(file_path_suffix);
+  std::ofstream file_stream(file_name);
+  file_stream << file_contents;
+  file_stream.close();
+  ASSERT_TRUE(std::filesystem::exists(file_name));
+}
+
+TEST(CreateFromFile, SucceedsOnValidAdmFile) {
+  std::string adm_file_name;
+  CreateFileFromStringView(kAdmBwfWithOneStereoAndOneMonoObject, ".adm",
+                           adm_file_name);
+
+  EXPECT_THAT(
+      WavReader::CreateFromFile(adm_file_name, kArbitraryNumSamplesPerFrame),
+      IsOk());
+}
 
 TEST(CreateFromFile, SucceedsOnValidWavFile) {
   const auto input_wav_file = std::filesystem::current_path() /
@@ -89,20 +143,28 @@ WavReader InitAndValidate(const std::filesystem::path& filename,
 }
 
 TEST(WavReader, GetNumChannelsMatchesWavFile) {
-  const size_t kNumSamplesPerFrame = 8;
+  EXPECT_EQ(InitAndValidate("stereo_8_samples_48khz_s16le.wav",
+                            kArbitraryNumSamplesPerFrame)
+                .num_channels(),
+            2);
+  EXPECT_EQ(InitAndValidate("stereo_8_samples_48khz_s24le.wav",
+                            kArbitraryNumSamplesPerFrame)
+                .num_channels(),
+            2);
+  EXPECT_EQ(InitAndValidate("sine_1000_16khz_512ms_s32le.wav",
+                            kArbitraryNumSamplesPerFrame)
+                .num_channels(),
+            1);
+}
 
-  EXPECT_EQ(
-      InitAndValidate("stereo_8_samples_48khz_s16le.wav", kNumSamplesPerFrame)
-          .num_channels(),
-      2);
-  EXPECT_EQ(
-      InitAndValidate("stereo_8_samples_48khz_s24le.wav", kNumSamplesPerFrame)
-          .num_channels(),
-      2);
-  EXPECT_EQ(
-      InitAndValidate("sine_1000_16khz_512ms_s32le.wav", kNumSamplesPerFrame)
-          .num_channels(),
-      1);
+TEST(WavReader, GetNumChannelsMatchesAdmFile) {
+  auto adm_file_name = GetAndCleanupOutputFileName(".adm");
+  CreateFileFromStringView(kAdmBwfWithOneStereoAndOneMonoObject, ".adm",
+                           adm_file_name);
+
+  EXPECT_EQ(InitAndValidate(adm_file_name, kArbitraryNumSamplesPerFrame)
+                .num_channels(),
+            3);
 }
 
 TEST(WavReader, GetSampleRateHzMatchesWavFile) {
@@ -122,6 +184,16 @@ TEST(WavReader, GetSampleRateHzMatchesWavFile) {
       16000);
 }
 
+TEST(WavReader, GetSampleRateHzMatchesAdmFile) {
+  auto adm_file_name = GetAndCleanupOutputFileName(".adm");
+  CreateFileFromStringView(kAdmBwfWithOneStereoAndOneMonoObject, ".adm",
+                           adm_file_name);
+
+  EXPECT_EQ(InitAndValidate(adm_file_name, kArbitraryNumSamplesPerFrame)
+                .sample_rate_hz(),
+            48000);
+}
+
 TEST(WavReader, GetBitDepthMatchesWavFile) {
   const size_t kNumSamplesPerFrame = 8;
 
@@ -139,6 +211,16 @@ TEST(WavReader, GetBitDepthMatchesWavFile) {
       32);
 }
 
+TEST(WavReader, GetBitDepthMatchesAdmFile) {
+  auto adm_file_name = GetAndCleanupOutputFileName(".adm");
+  CreateFileFromStringView(kAdmBwfWithOneStereoAndOneMonoObject, ".adm",
+                           adm_file_name);
+
+  EXPECT_EQ(
+      InitAndValidate(adm_file_name, kArbitraryNumSamplesPerFrame).bit_depth(),
+      16);
+}
+
 TEST(WavReader, GetNumRemainingSamplesUpdatesWithRead) {
   // Read four samples x two channels per frame.
   const size_t kNumSamplesPerFrame = 4;
@@ -147,6 +229,23 @@ TEST(WavReader, GetNumRemainingSamplesUpdatesWithRead) {
   EXPECT_EQ(wav_reader.remaining_samples(), 16);
   wav_reader.ReadFrame();
   EXPECT_EQ(wav_reader.remaining_samples(), 8);
+  wav_reader.ReadFrame();
+  EXPECT_EQ(wav_reader.remaining_samples(), 0);
+}
+
+TEST(WavReader, GetNumRemainingSamplesMatchesUpdatesWithReadForAdm) {
+  // Read one sample x three channels per frame.
+  const size_t kNumSamplesPerFrame = 1;
+
+  auto adm_file_name = GetAndCleanupOutputFileName(".adm");
+  CreateFileFromStringView(kAdmBwfWithOneStereoAndOneMonoObject, ".adm",
+                           adm_file_name);
+
+  auto wav_reader = InitAndValidate(adm_file_name, kNumSamplesPerFrame);
+
+  EXPECT_EQ(wav_reader.remaining_samples(), 6);
+  wav_reader.ReadFrame();
+  EXPECT_EQ(wav_reader.remaining_samples(), 3);
   wav_reader.ReadFrame();
   EXPECT_EQ(wav_reader.remaining_samples(), 0);
 }
@@ -220,6 +319,23 @@ TEST(WavReader, OneFrame32BitLittleEndian) {
       {214748364}, {198401618}, {151850024}, {82180641},
   };
   EXPECT_EQ(wav_reader.buffers_, expected_frame);
+}
+
+TEST(WavReader, OneFrameAdm) {
+  const size_t kNumSamplesPerFrame = 1;
+
+  auto adm_file_name = GetAndCleanupOutputFileName(".adm");
+  CreateFileFromStringView(kAdmBwfWithOneStereoAndOneMonoObject, ".adm",
+                           adm_file_name);
+
+  auto wav_reader = InitAndValidate(adm_file_name, kNumSamplesPerFrame);
+
+  // Read one frame. The result of n-bit samples are stored in the upper `n`
+  // bits.
+  EXPECT_EQ(wav_reader.ReadFrame(), 3);
+  const std::vector<std::vector<int32_t>> kExpectedFrame = {
+      {0x23010000, 0x67450000, static_cast<int32_t>(0xbbaa0000)}};
+  EXPECT_EQ(wav_reader.buffers_, kExpectedFrame);
 }
 
 TEST(WavReader, IsSafeToCallReadFrameAfterMove) {
