a3d8d2f014543e09e5ee08c2237aef67e050ade1
jwcullen <jwcullen@google.com>
2025-03-17 16:14:00 -0400

    No public description
    PiperOrigin-RevId: 737726816

--- external/git_status_command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# A script designed to work with bazel's workspace status command
-# (https://bazel.build/docs/user-manual#workspace-status). This script prints
-# output in key-value pairs.
-#   - {BUILD_CHANGELIST, git commit hash or "unknown" if it fails}
-
-echo -n "BUILD_CHANGELIST "
-echo "$(git rev-parse HEAD)" || echo "unknown"
