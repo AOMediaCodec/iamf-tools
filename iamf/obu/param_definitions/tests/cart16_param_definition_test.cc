f7e7089c2fffe344a6f48f6a13231cd687a69ae3
Googler <copybara-servicebot@google.com>
2025-12-12 16:38:47 -0500

    Add Cart16ParamDefinition class and tests.
    Introduces `Cart16ParamDefinition` to handle parameter definitions for Cartesian coordinates using 16-bit signed integers. Implements methods to read, write, and validate the default x, y, and z values. Adds unit tests for reading, writing, and creating parameter data.
    
    PiperOrigin-RevId: 843815945

---
+++ iamf/obu/param_definitions/tests/cart16_param_definition_test.cc
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 3-Clause Clear License
+ * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
+ * License was not distributed with this source code in the LICENSE file, you
+ * can obtain it at www.aomedia.org/license/software-license/bsd-3-c-c. If the
+ * Alliance for Open Media Patent License 1.0 was not distributed with this
+ * source code in the PATENTS file, you can obtain it at
+ * www.aomedia.org/license/patent.
+ */
+#include "iamf/obu/param_definitions/cart16_param_definition.h"
+
+#include <cstdint>
+#include <vector>
+
+#include "absl/status/status_matchers.h"
+#include "absl/types/span.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "iamf/common/read_bit_buffer.h"
+#include "iamf/common/utils/tests/test_utils.h"
+#include "iamf/common/write_bit_buffer.h"
+#include "iamf/obu/cart16_parameter_data.h"
+#include "iamf/obu/param_definitions.h"
+
+namespace iamf_tools {
+namespace {
+
+using ::absl_testing::IsOk;
+
+constexpr int32_t kBufferSize = 256;
+
+void PopulateParamDefinition(ParamDefinition* param_definition) {
+  param_definition->parameter_id_ = 1;
+  param_definition->parameter_rate_ = 1;
+  param_definition->param_definition_mode_ = 0;
+  param_definition->duration_ = 10;
+  param_definition->constant_subblock_duration_ = 10;
+  param_definition->reserved_ = 0;
+}
+
+TEST(Cart16ParamDefinitionTest, GetType) {
+  Cart16ParamDefinition param_definition;
+  EXPECT_EQ(param_definition.GetType(),
+            ParamDefinition::kParameterDefinitionCart16);
+}
+
+TEST(Cart16ParamDefinitionTest, ReadAndValidateSucceeds) {
+  Cart16ParamDefinition param_definition;
+  std::vector<uint8_t> data = {1,   // parameter_id
+                               1,   // parameter_rate
+                               0,   // mode
+                               10,  // duration
+                               10,  // constant_subblock_duration
+                               // default_x = 1 (16 bits)
+                               // default_y = 2 (16 bits)
+                               // default_z = 3 (16 bits)
+                               0x00, 0x01, 0x00, 0x02, 0x00, 0x03};
+
+  auto rb = MemoryBasedReadBitBuffer::CreateFromSpan(data);
+  EXPECT_THAT(param_definition.ReadAndValidate(*rb), IsOk());
+  EXPECT_EQ(param_definition.parameter_id_, 1);
+  EXPECT_EQ(param_definition.parameter_rate_, 1);
+  EXPECT_EQ(param_definition.param_definition_mode_, 0);
+  EXPECT_EQ(param_definition.duration_, 10);
+  EXPECT_EQ(param_definition.constant_subblock_duration_, 10);
+  EXPECT_EQ(param_definition.default_x_, 1);
+  EXPECT_EQ(param_definition.default_y_, 2);
+  EXPECT_EQ(param_definition.default_z_, 3);
+}
+
+TEST(Cart16ParamDefinitionTest, WriteAndValidateSucceeds) {
+  Cart16ParamDefinition param_definition;
+  PopulateParamDefinition(&param_definition);
+  param_definition.default_x_ = 1;
+  param_definition.default_y_ = 2;
+  param_definition.default_z_ = 3;
+
+  std::vector<uint8_t> expected_data = {1,   // parameter_id
+                                        1,   // parameter_rate
+                                        0,   // mode
+                                        10,  // duration
+                                        10,  // constant_subblock_duration
+                                             // default_x = 1 (16 bits)
+                                        // default_y = 2 (16 bits)
+                                        // default_z = 3 (16 bits)
+                                        0x00, 0x01, 0x00, 0x02, 0x00, 0x03};
+  WriteBitBuffer wb(kBufferSize);
+  EXPECT_THAT(param_definition.ValidateAndWrite(wb), IsOk());
+  ValidateWriteResults(wb, expected_data);
+}
+
+TEST(Cart16ParamDefinitionTest, CreateParameterDataReturnsNonNull) {
+  Cart16ParamDefinition param_definition;
+  EXPECT_NE(param_definition.CreateParameterData(), nullptr);
+}
+
+}  // namespace
+}  // namespace iamf_tools
